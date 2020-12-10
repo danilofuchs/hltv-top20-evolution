6599bf2e36303ca0acdec2f454d87964f54c8de4
Danilo Campana Fuchs <danilo.fuchs@ebanx.com>
2020-12-09 21:40:05 -0300

    feat: Initial work on showing ranks

--- src/components/BumpChart.tsx
+++ src/components/BumpChart.tsx
@@ -1,29 +1,59 @@
-import { Bump } from "@nivo/bump";
+import { Bump, BumpInputSerie } from "@nivo/bump";
+import players from "@src/data/players.json";
+import { Player } from "@src/entities/player";
+
+const data: BumpInputSerie[] = (players as Player[]).map((player) => ({
+  id: player.name,
+  name: player.name,
+  data: [
+    {
+      x: 2013,
+      y: player.rankings[2013],
+    },
+    {
+      x: 2014,
+      y: player.rankings[2014],
+    },
+    {
+      x: 2015,
+      y: player.rankings[2015],
+    },
+    {
+      x: 2016,
+      y: player.rankings[2016],
+    },
+    {
+      x: 2017,
+      y: player.rankings[2017],
+    },
+    {
+      x: 2018,
+      y: player.rankings[2018],
+    },
+    {
+      x: 2019,
+      y: player.rankings[2019],
+    },
+  ],
+}));
 
 interface Props {}
 export function BumpChart(props: Props) {
   return (
     <Bump
-      data={[
-        {
-          id: "1",
-          data: [
-            { x: 1, y: 2 },
-            { x: 2, y: 1 },
-          ],
-          teste: 2,
-        },
-        {
-          id: "2",
-          data: [
-            { x: 1, y: 1 },
-            { x: 2, y: 2 },
-          ],
-        },
-      ]}
+      data={data}
       height={400}
       width={800}
+      margin={{ top: 40, right: 100, bottom: 40, left: 60 }}
       enableGridY={false}
+      axisBottom={{
+        tickSize: 5,
+        tickPadding: 5,
+        tickRotation: 0,
+        legend: "",
+        legendPosition: "middle",
+        legendOffset: 32,
+      }}
     />
   );
 }
