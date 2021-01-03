1f74d5bd8b176c290ca782f292874a2b3a30b37b
Danilo Campana Fuchs <danilo.fuchs@ebanx.com>
2021-01-02 21:23:07 -0300

    WIP: react-vis

--- src/components/BumpChart.tsx
+++ src/components/BumpChart.tsx
@@ -1,59 +1,48 @@
-import { Bump, BumpInputSerie } from "@nivo/bump";
-import players from "@src/data/players.json";
+import {
+  XYPlot,
+  LineSeries,
+  LabelSeries,
+  VerticalGridLines,
+  HorizontalGridLines,
+  XAxis,
+  YAxis,
+} from "react-vis";
+import playersJson from "@src/data/players.json";
 import { Player } from "@src/entities/player";
 
-const data: BumpInputSerie[] = (players as Player[]).map((player) => ({
-  id: player.name,
-  name: player.name,
-  data: [
-    {
-      x: 2013,
-      y: player.rankings[2013],
-    },
-    {
-      x: 2014,
-      y: player.rankings[2014],
-    },
-    {
-      x: 2015,
-      y: player.rankings[2015],
-    },
-    {
-      x: 2016,
-      y: player.rankings[2016],
-    },
-    {
-      x: 2017,
-      y: player.rankings[2017],
-    },
-    {
-      x: 2018,
-      y: player.rankings[2018],
-    },
-    {
-      x: 2019,
-      y: player.rankings[2019],
-    },
-  ],
-}));
+const players = playersJson as Player[];
+
+const years = new Set(players[0].rankings.map((rank) => rank.year));
+
+const yTickValues = new Array(20).fill(0).map((_, i) => i + 1);
+const xTickValues = Array.from(years.values()).sort((a, b) => a - b);
+
+const buildDataForPlayer = (player: Player): any[] => {
+  return player.rankings.map((rank) => ({
+    x: rank.year,
+    y: rank.place as number,
+    // label: player.name,
+  }));
+};
 
 interface Props {}
 export function BumpChart(props: Props) {
   return (
-    <Bump
-      data={data}
-      height={400}
-      width={800}
-      margin={{ top: 40, right: 100, bottom: 40, left: 60 }}
-      enableGridY={false}
-      axisBottom={{
-        tickSize: 5,
-        tickPadding: 5,
-        tickRotation: 0,
-        legend: "",
-        legendPosition: "middle",
-        legendOffset: 32,
-      }}
-    />
+    <XYPlot height={400} width={800} yDomain={[20, 1]} margin={40}>
+      <VerticalGridLines tickValues={xTickValues} />
+      <HorizontalGridLines tickValues={yTickValues} />
+      <XAxis tickValues={xTickValues} tickFormat={(tick) => tick.toString()} />
+      <YAxis tickValues={yTickValues} />
+      {players.map((player) => (
+        <LineSeries
+          data={buildDataForPlayer(player)}
+          getNull={(d) => d.y !== null}
+          curve={"curveMonotoneX"}
+        />
+      ))}
+      {players.map((player) => (
+        <LabelSeries data={buildDataForPlayer(player)} />
+      ))}
+    </XYPlot>
   );
 }
