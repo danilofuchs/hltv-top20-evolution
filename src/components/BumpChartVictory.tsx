e3be20773986038d801a9511f0ac4e975037d15b
Danilo Campana Fuchs <danilo.fuchs@ebanx.com>
2021-01-03 17:13:40 -0300

    fix: Axis labels

--- src/components/BumpChartVictory.tsx
+++ src/components/BumpChartVictory.tsx
@@ -1,5 +1,4 @@
 import {
-  Curve,
   VictoryAxis,
   VictoryChart,
   VictoryLine,
@@ -12,19 +11,6 @@ import React from "react";
 
 const players = playersJson as Player[];
 
-const years = new Set(players[0].rankings.map((rank) => rank.year));
-
-const yTickValues = new Array(20).fill(0).map((_, i) => i + 1);
-const xTickValues = Array.from(years.values()).sort((a, b) => a - b);
-
-const buildDataForPlayer = (player: Player): any[] => {
-  return player.rankings.map((rank) => ({
-    x: rank.year,
-    y: rank.place as number,
-    // label: player.name,
-  }));
-};
-
 interface Props {}
 export function BumpChartVictory(props: Props) {
   return (
@@ -33,25 +19,22 @@ export function BumpChartVictory(props: Props) {
       width={800}
       domain={{ y: [20, 1] }}
       containerComponent={
-        <VictoryVoronoiContainer
-          portalZIndex={1000}
-          mouseFollowTooltips
-          // labels={({ datum }) => `${datum.year}, ${datum.place}`}
-          // labelComponent={<VictoryTooltip dy={-7} constrainToVisibleArea />}
-        />
+        <VictoryVoronoiContainer portalZIndex={1000} mouseFollowTooltips />
       }
     >
-      {/* <VerticalGridLines tickValues={xTickValues} />
-      <HorizontalGridLines tickValues={yTickValues} />
-      <XAxis tickValues={xTickValues} tickFormat={(tick) => tick.toString()} />
-      <YAxis tickValues={yTickValues} /> */}
+      <VictoryAxis dependentAxis tickCount={20} />
+      <VictoryAxis tickFormat={(year) => year.toString()} />
       {players.map((player) => (
         <VictoryLine
           data={player.rankings}
           x="year"
           y="place"
           labels={() => player.name}
-          dataComponent={<Curve />}
+          style={{
+            data: {
+              stroke: (args) => (args.active ? "tomato" : "black"),
+            },
+          }}
           labelComponent={
             <VictoryTooltip
               flyoutStyle={{
