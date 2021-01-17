ae8dc446cf7d3ca24deb41c692dd85da523ece5e
Danilo Campana Fuchs <danilo.fuchs@ebanx.com>
2021-01-17 20:25:23 -0300

    Add GA tracking

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -31,11 +31,21 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap"
             rel="stylesheet"
           />
-          <link
-            href="https://unpkg.com/react-vis/dist/style.css"
-            rel="stylesheet"
-            key="test"
-          />
+          {/* Global site tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-S3NKNK22NJ"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+
+                  gtag('config', 'G-S3NKNK22NJ');`,
+            }}
+          ></script>
         </Head>
         <body>
           <Main />
