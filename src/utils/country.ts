9d32a7a5ab9dcc905b1e1c51d6627b933825d3c4
Danilo Campana Fuchs <danilo.fuchs@ebanx.com>
2021-01-17 20:13:22 -0300

    Country Chart [WIP]

--- src/utils/country.ts
+++ src/utils/country.ts
@@ -1,3 +1,20 @@
+import { countries } from "countries-list";
+
+type CountryCode = keyof typeof countries;
+
+export function getCountryName(countryCode: string) {
+  return (
+    countries[countryCode.toUpperCase().trim() as CountryCode]?.name || null
+  );
+}
+
+export function getCountryContinent(countryCode: string) {
+  return (
+    countries[countryCode.toUpperCase().trim() as CountryCode]?.continent ||
+    null
+  );
+}
+
 export function getCountryFlagSrc(countryCode: string) {
   return `https://cdn.jsdelivr.net/gh/madebybowtie/FlagKit@2.2/Assets/SVG/${countryCode}.svg`;
 }
