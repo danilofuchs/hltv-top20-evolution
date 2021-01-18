41cd52375924a518ca91f89d57be725e2098ff70
Danilo Campana Fuchs <danilo.fuchs@ebanx.com>
2021-01-17 22:05:55 -0300

    fix: Faded colors

--- src/utils/color.ts
+++ src/utils/color.ts
@@ -1,7 +1,17 @@
 import seedRandom from "seedrandom";
+import color from "color";
 
 export function getPlayerColor(playerName: string) {
   const rng = seedRandom(playerName);
 
-  return "#" + Math.floor(rng() * 16777215).toString(16);
+  const randomColor =
+    "#" +
+    Math.floor(rng() * 16777215)
+      .toString(16)
+      .padEnd(6, "0");
+
+  if (color(randomColor).lightness() > 60) {
+    return color(randomColor).darken(0.3);
+  }
+  return randomColor;
 }
